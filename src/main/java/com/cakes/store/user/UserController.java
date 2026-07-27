7e7f8ef3ad8099d4dfdd76377965ff6de4c4c2d9
Aline Sugisaki <aline_spinelly@hotmail.com>
2026-07-27 14:09:39 +0900

    Adjusting the updateRole

--- src/main/java/com/cakes/store/user/UserController.java
+++ src/main/java/com/cakes/store/user/UserController.java
@@ -3,6 +3,7 @@ package com.cakes.store.user;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -24,6 +25,8 @@ public class UserController {
         return ResponseEntity.created(adress).body(userDTO);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("role/{id}")
     public ResponseEntity<UserData> updateRole(@PathVariable Long id, @RequestBody @Valid RoleUpdateDTO dto){
         UserData updated = userService.updateRole(id, dto);
         return ResponseEntity.ok(updated);
